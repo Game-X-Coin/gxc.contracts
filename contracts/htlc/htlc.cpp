3445c6c7aec35fd150f24f15a6ab3fa44a8a121e
Jeeyong Um <conr2d@gmail.com>
2019-07-16 07:22:32 +0000

    Issue new token when token is transferred from ETH

--- contracts/htlc/htlc.cpp
+++ contracts/htlc/htlc.cpp
@@ -8,24 +8,26 @@ namespace gxc {
 
 using esc = sio4::extended_symbol_code;
 
+constexpr name system_account{"gxc"_n};
+constexpr name null_account{"gxc.null"_n};
 constexpr name vault_account{"gxc.vault"_n};
 
 void htlc::newcontract(name owner, string contract_name, std::variant<name, checksum160> recipient, extended_asset value, checksum256 hashlock, time_point_sec timelock) {
    require_auth(owner);
+
+   check(std::holds_alternative<checksum160>(recipient) || (owner == vault_account), "invalid recipient");
 
    htlc_index idx(_self, owner.value);
    check(idx.find(std::hash<std::string>()(contract_name)) == idx.end(), "existing contract name");
 
    config_index cfg(_self, _self.value);
    auto it = cfg.find(std::hash<esc>()(esc{value.quantity.symbol.code(), value.contract}));
-
    bool constrained = owner != vault_account && it != cfg.end();
-
    auto min_amount = (constrained) ? it->min_amount : extended_asset(0, value.get_extended_symbol());
    auto min_duration = (constrained) ? it->min_duration : 0;
 
    check(value >= min_amount, "specified amount is not enough");
-   check(timelock >= current_time_point() + microseconds(static_cast<int64_t>(min_duration * 1000)), "the expiration time should be in the future");
+   check(timelock >= current_time_point() + seconds(min_duration), "the expiration time should be in the future");
 
    idx.emplace(owner, [&](auto& l) {
       l.contract_name = contract_name;
@@ -35,9 +37,11 @@ void htlc::newcontract(name owner, string contract_name, std::variant<name, chec
       l.timelock = timelock;
    });
 
-   token _token;
-   _token.authorization = {{_self, "active"_n}};
-   _token.transfer(owner, _self, value, "htlc created by " + owner.to_string() + (contract_name.size() ? ": " : "") + contract_name);
+   if (owner != vault_account) {
+      token _token;
+      _token.authorization = {{_self, "active"_n}};
+      _token.transfer(owner, _self, value, "htlc created by " + owner.to_string() + (contract_name.size() ? ": " : "") + contract_name);
+   }
 }
 
 void htlc::withdraw(name owner, string contract_name, checksum256 preimage) {
@@ -53,17 +57,23 @@ void htlc::withdraw(name owner, string contract_name, checksum256 preimage) {
    check(memcmp((const void*)it.hashlock.data(), (const void*)hash.data(), 32) == 0, "invalid preimage");
 
    token _token;
+   auto processed_msg = "htlc processed from " + owner.to_string() + (contract_name.size() ? ": " : "") + contract_name;
+
    if (std::holds_alternative<name>(it.recipient)) {
-      _token.transfer(_self, std::get<name>(it.recipient), it.value, "htlc processed from " + owner.to_string() + (contract_name.size() ? ": " : "") + contract_name);
+      check(owner == vault_account, "must not happen");
+      if (it.value.contract == system_account) {
+         _token.authorization = {{ system_account, "token"_n }};
+      }
+      _token.transfer(null_account, std::get<name>(it.recipient), it.value, processed_msg);
    } else {
-      _token.transfer(_self, vault_account, it.value, "htlc processed from " + owner.to_string() + (contract_name.size() ? ": " : "") + contract_name);
+      _token.transfer(_self, null_account, it.value, processed_msg);
    }
 
    idx.erase(it);
 }
 
 void htlc::refund(name owner, string contract_name) {
-   require_auth(owner);
+   //require_auth(owner);
 
    htlc_index idx(_self, owner.value);
    const auto& it = idx.get(std::hash<std::string>()(contract_name));
